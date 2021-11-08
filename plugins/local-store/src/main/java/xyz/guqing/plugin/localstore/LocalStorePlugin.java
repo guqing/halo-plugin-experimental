d6ab0c43d64214d2d6f02e9e57363fa7dfc687c0
guqing <1484563614@qq.com>
2021-11-08 17:42:22 +0800

    feat: 新增本地存储插件

---
+++ plugins/local-store/src/main/java/xyz/guqing/plugin/localstore/LocalStorePlugin.java
@@ -0,0 +1,16 @@
+package xyz.guqing.plugin.localstore;
+
+
+import org.pf4j.PluginWrapper;
+import run.halo.app.extensions.SpringPlugin;
+
+/**
+ * @author guqing
+ * @since 2021-11-06
+ */
+public class LocalStorePlugin extends SpringPlugin {
+
+    public LocalStorePlugin(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+}
