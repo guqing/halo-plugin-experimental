1e666b6acfb4eaa8aeb86e35bf5d0a015799156a
guqing <1484563614@qq.com>
2022-05-15 00:17:27 +0800

    refactor: remove SingletonSpringExtensionFactory

--- core/src/main/java/run/halo/app/extensions/SpringPlugin.java
+++ core/src/main/java/run/halo/app/extensions/SpringPlugin.java
@@ -3,7 +3,6 @@ package run.halo.app.extensions;
 import lombok.extern.slf4j.Slf4j;
 import org.pf4j.Plugin;
 import org.pf4j.PluginWrapper;
-import org.springframework.context.ApplicationContext;
 
 @Slf4j
 public abstract class SpringPlugin extends Plugin {
