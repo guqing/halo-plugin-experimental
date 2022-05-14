adff846951aba2000b026ef4d994250c4bab9848
guqing <1484563614@qq.com>
2022-05-15 00:27:20 +0800

    feat: add java doc

--- core/src/main/java/run/halo/app/extensions/SpringPlugin.java
+++ core/src/main/java/run/halo/app/extensions/SpringPlugin.java
@@ -14,6 +14,10 @@ public abstract class SpringPlugin extends Plugin {
     }
 
     /**
+     * <p>Lazy initialization plugin application context,
+     * avoid being unable to get context when system start scan plugin.</p>
+     * <p>The plugin application context is not created until the plug-in is started.</p>
+     *
      * @return Plugin application context.
      */
     public synchronized final PluginApplicationContext getApplicationContext() {
